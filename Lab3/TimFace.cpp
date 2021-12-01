3b87c9f683ba6b90180d44ea72dd6ade4cfdbc7d
TimSong <3190101087@zju.edu.cn>
2021-12-01 16:22:38 +0800

    Lab3 start

---
+++ Lab3/TimFace.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include <opencv2/opencv.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
+
+int main()
+{
+    
+}
---
+++ Lab3/compile.bat
@@ -0,0 +1,1 @@
+g++ TimFace.cpp -o Face.exe -I C:\opencv\build\include -L C:\opencv\build\x64\MinGW\lib -lopencv_core454 -lopencv_imgproc454 -lopencv_imgcodecs454 -lopencv_video454 -lopencv_ml454 -lopencv_highgui454 -lopencv_objdetect454 -lopencv_flann454 -lopencv_imgcodecs454 -lopencv_photo454 -lopencv_videoio454
